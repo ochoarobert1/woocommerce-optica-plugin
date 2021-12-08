5a679dd662807afba7fe21e4659f1f9141e2a47c
Robert Ochoa <ochoa.robert1@gmail.com>
2021-12-08 19:10:34 -0400

    Seleccion de atributos a usar

--- admin/class-woooptica-admin.php
+++ admin/class-woooptica-admin.php
@@ -1,15 +1,4 @@
 <?php
-
-/**
- * The admin-specific functionality of the plugin.
- *
- * @link       http://example.com
- * @since      1.0.0
- *
- * @package    WooOptica
- * @subpackage WooOptica/admin
- */
-
 /**
  * The admin-specific functionality of the plugin.
  *
@@ -61,19 +50,6 @@ class WooOptica_Admin
      */
     public function enqueue_styles()
     {
-
-        /**
-         * This function is provided for demonstration purposes only.
-         *
-         * An instance of this class should be passed to the run() function
-         * defined in WooOptica_Loader as all of the hooks are defined
-         * in that particular class.
-         *
-         * The WooOptica_Loader will then create the relationship
-         * between the defined hooks and the functions defined in this
-         * class.
-         */
-
         wp_enqueue_style($this->woooptica, plugin_dir_url(__FILE__) . 'css/woooptica-admin.css', array(), $this->version, 'all');
     }
 
@@ -84,19 +60,109 @@ class WooOptica_Admin
      */
     public function enqueue_scripts()
     {
-
-        /**
-         * This function is provided for demonstration purposes only.
-         *
-         * An instance of this class should be passed to the run() function
-         * defined in WooOptica_Loader as all of the hooks are defined
-         * in that particular class.
-         *
-         * The WooOptica_Loader will then create the relationship
-         * between the defined hooks and the functions defined in this
-         * class.
-         */
-
         wp_enqueue_script($this->woooptica, plugin_dir_url(__FILE__) . 'js/woooptica-admin.js', array( 'jquery' ), $this->version, false);
+    }
+
+    /**
+     * Register a custom data tab for Woocommerce
+     *
+     * @param $product_data_tabs $product_data_tabs [array]
+     * @since    1.0.0
+     *
+     */
+    public function custom_product_data_tab_handler($product_data_tabs)
+    {
+        // Modify product data tabs array
+        $product_data_tabs['optica-tab'] = array(
+            'label'     => __('Datos de Lentes', 'woo-optica'),
+            'target'    => 'custom_optica_product_data',
+            'priority'  => 15,
+            'class'     => array( 'show_if_simple', 'show_if_variable' ),
+        );
+
+        return $product_data_tabs;
+    }
+
+    /**
+     * Add custom data to tab for Woocommerce
+     *
+     * @param   null
+     * @since   1.0.0
+     *
+     */
+    public function custom_product_data_fields()
+    {
+        global $post;
+        ob_start(); ?>
+<div id='custom_optica_product_data' class='panel woocommerce_options_panel'>
+    <div class='options_group'>
+        <?php
+
+        // Get an array of product attribute taxonomies slugs
+        $attributes_tax_slugs = array_keys(wc_get_attribute_taxonomy_labels());
+
+        // Get an array of product attribute taxonomies names (starting with "pa_")
+        $attributes_tax_names = array_filter(array_map('wc_attribute_taxonomy_name', $attributes_tax_slugs));
+
+        $data_select = array();
+        foreach ($attributes_tax_slugs as $key => $value) {
+            $data_select[$attributes_tax_names[$key]] = $value;
+        }
+        
+        woocommerce_wp_text_input(
+            array(
+              'id' => '_select_attributes',
+              'label' => __('Selecciona las características de los lentes a usar', 'woooptica'),
+              'options' => $data_select
+            )
+        ); ?>
+        </div>
+</div>
+<?php
+        $content = ob_get_clean();
+        echo $content;
+    }
+
+    /**
+     * Saving custom data to product
+     *
+     * @param   $post_id [Integer]
+     * @since   1.0.0
+     *
+     */
+    public function save_custom_data_fields($post_id)
+    {
+        // Save Text Field
+        $text_field = $_POST['_text_field'];
+        if (!empty($text_field)) {
+            update_post_meta($post_id, '_text_field', esc_attr($text_field));
+        }
+    
+        // Save Number Field
+        $number_field = $_POST['_number_field'];
+        if (!empty($number_field)) {
+            update_post_meta($post_id, '_number_field', esc_attr($number_field));
+        }
+        // Save Textarea
+        $textarea = $_POST['_textarea'];
+        if (!empty($textarea)) {
+            update_post_meta($post_id, '_textarea', esc_html($textarea));
+        }
+    
+        // Save Select
+        $select = $_POST['_select'];
+        if (!empty($select)) {
+            update_post_meta($post_id, '_select', esc_attr($select));
+        }
+    
+        // Save Checkbox
+        $checkbox = isset($_POST['_checkbox']) ? 'yes' : 'no';
+        update_post_meta($post_id, '_checkbox', $checkbox);
+    
+        // Save Hidden field
+        $hidden = $_POST['_hidden_field'];
+        if (!empty($hidden)) {
+            update_post_meta($post_id, '_hidden_field', esc_attr($hidden));
+        }
     }
 }
--- includes/class-woooptica.php
+++ includes/class-woooptica.php
@@ -27,192 +27,200 @@
  * @subpackage WooOptica/includes
  * @author     Robert Ochoa <contacto@robertochoaweb.com>
  */
-class WooOptica {
+class WooOptica
+{
 
-	/**
-	 * The loader that's responsible for maintaining and registering all hooks that power
-	 * the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      WooOptica_Loader    $loader    Maintains and registers all hooks for the plugin.
-	 */
-	protected $loader;
+    /**
+     * The loader that's responsible for maintaining and registering all hooks that power
+     * the plugin.
+     *
+     * @since    1.0.0
+     * @access   protected
+     * @var      WooOptica_Loader    $loader    Maintains and registers all hooks for the plugin.
+     */
+    protected $loader;
 
-	/**
-	 * The unique identifier of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $woooptica    The string used to uniquely identify this plugin.
-	 */
-	protected $woooptica;
+    /**
+     * The unique identifier of this plugin.
+     *
+     * @since    1.0.0
+     * @access   protected
+     * @var      string    $woooptica    The string used to uniquely identify this plugin.
+     */
+    protected $woooptica;
 
-	/**
-	 * The current version of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $version    The current version of the plugin.
-	 */
-	protected $version;
+    /**
+     * The current version of the plugin.
+     *
+     * @since    1.0.0
+     * @access   protected
+     * @var      string    $version    The current version of the plugin.
+     */
+    protected $version;
 
-	/**
-	 * Define the core functionality of the plugin.
-	 *
-	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the admin area and
-	 * the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function __construct() {
-		if ( defined( 'WOO_OPTICA_VERSION' ) ) {
-			$this->version = WOO_OPTICA_VERSION;
-		} else {
-			$this->version = '1.0.0';
-		}
-		$this->woooptica = 'woooptica';
+    /**
+     * Define the core functionality of the plugin.
+     *
+     * Set the plugin name and the plugin version that can be used throughout the plugin.
+     * Load the dependencies, define the locale, and set the hooks for the admin area and
+     * the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function __construct()
+    {
+        if (defined('WOO_OPTICA_VERSION')) {
+            $this->version = WOO_OPTICA_VERSION;
+        } else {
+            $this->version = '1.0.0';
+        }
+        $this->woooptica = 'woooptica';
 
-		$this->load_dependencies();
-		$this->set_locale();
-		$this->define_admin_hooks();
-		$this->define_public_hooks();
+        $this->load_dependencies();
+        $this->set_locale();
+        $this->define_admin_hooks();
+        $this->define_public_hooks();
+    }
 
-	}
+    /**
+     * Load the required dependencies for this plugin.
+     *
+     * Include the following files that make up the plugin:
+     *
+     * - WooOptica_Loader. Orchestrates the hooks of the plugin.
+     * - WooOptica_i18n. Defines internationalization functionality.
+     * - WooOptica_Admin. Defines all hooks for the admin area.
+     * - WooOptica_Public. Defines all hooks for the public side of the site.
+     *
+     * Create an instance of the loader which will be used to register the hooks
+     * with WordPress.
+     *
+     * @since    1.0.0
+     * @access   private
+     */
+    private function load_dependencies()
+    {
 
-	/**
-	 * Load the required dependencies for this plugin.
-	 *
-	 * Include the following files that make up the plugin:
-	 *
-	 * - WooOptica_Loader. Orchestrates the hooks of the plugin.
-	 * - WooOptica_i18n. Defines internationalization functionality.
-	 * - WooOptica_Admin. Defines all hooks for the admin area.
-	 * - WooOptica_Public. Defines all hooks for the public side of the site.
-	 *
-	 * Create an instance of the loader which will be used to register the hooks
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function load_dependencies() {
+        /**
+         * The class responsible for orchestrating the actions and filters of the
+         * core plugin.
+         */
+        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-woooptica-loader.php';
 
-		/**
-		 * The class responsible for orchestrating the actions and filters of the
-		 * core plugin.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-woooptica-loader.php';
+        /**
+         * The class responsible for defining internationalization functionality
+         * of the plugin.
+         */
+        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-woooptica-i18n.php';
 
-		/**
-		 * The class responsible for defining internationalization functionality
-		 * of the plugin.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-woooptica-i18n.php';
+        /**
+         * The class responsible for defining all actions that occur in the admin area.
+         */
+        require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-woooptica-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the admin area.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-woooptica-admin.php';
+        /**
+         * The class responsible for defining all actions that occur in the public-facing
+         * side of the site.
+         */
+        require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-woooptica-public.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-woooptica-public.php';
+        $this->loader = new WooOptica_Loader();
+    }
 
-		$this->loader = new WooOptica_Loader();
+    /**
+     * Define the locale for this plugin for internationalization.
+     *
+     * Uses the WooOptica_i18n class in order to set the domain and to register the hook
+     * with WordPress.
+     *
+     * @since    1.0.0
+     * @access   private
+     */
+    private function set_locale()
+    {
+        $plugin_i18n = new WooOptica_i18n();
 
-	}
+        $this->loader->add_action('plugins_loaded', $plugin_i18n, 'load_plugin_textdomain');
+    }
 
-	/**
-	 * Define the locale for this plugin for internationalization.
-	 *
-	 * Uses the WooOptica_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function set_locale() {
+    /**
+     * Register all of the hooks related to the admin area functionality
+     * of the plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     */
+    private function define_admin_hooks()
+    {
+        $plugin_admin = new WooOptica_Admin($this->get_woooptica(), $this->get_version());
 
-		$plugin_i18n = new WooOptica_i18n();
+        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles', 99);
+        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts', 99);
 
-		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+        // Woocommerce admin hooks
 
-	}
+		// Custom Woocommerce data tabs
+        $this->loader->add_filter('woocommerce_product_data_tabs', $plugin_admin, 'custom_product_data_tab_handler');
+        $this->loader->add_action('woocommerce_product_data_panels', $plugin_admin, 'custom_product_data_fields');
+        $this->loader->add_action('woocommerce_process_product_meta_simple', $plugin_admin, 'save_custom_data_fields');
+    }
 
-	/**
-	 * Register all of the hooks related to the admin area functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_admin_hooks() {
+    /**
+     * Register all of the hooks related to the public-facing functionality
+     * of the plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     */
+    private function define_public_hooks()
+    {
+        $plugin_public = new WooOptica_Public($this->get_woooptica(), $this->get_version());
 
-		$plugin_admin = new WooOptica_Admin( $this->get_woooptica(), $this->get_version() );
+        $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
+        $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
+    }
 
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+    /**
+     * Run the loader to execute all of the hooks with WordPress.
+     *
+     * @since    1.0.0
+     */
+    public function run()
+    {
+        $this->loader->run();
+    }
 
-	}
+    /**
+     * The name of the plugin used to uniquely identify it within the context of
+     * WordPress and to define internationalization functionality.
+     *
+     * @since     1.0.0
+     * @return    string    The name of the plugin.
+     */
+    public function get_woooptica()
+    {
+        return $this->woooptica;
+    }
 
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
+    /**
+     * The reference to the class that orchestrates the hooks with the plugin.
+     *
+     * @since     1.0.0
+     * @return    WooOptica_Loader    Orchestrates the hooks of the plugin.
+     */
+    public function get_loader()
+    {
+        return $this->loader;
+    }
 
-		$plugin_public = new WooOptica_Public( $this->get_woooptica(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
-	}
-
-	/**
-	 * Run the loader to execute all of the hooks with WordPress.
-	 *
-	 * @since    1.0.0
-	 */
-	public function run() {
-		$this->loader->run();
-	}
-
-	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The name of the plugin.
-	 */
-	public function get_woooptica() {
-		return $this->woooptica;
-	}
-
-	/**
-	 * The reference to the class that orchestrates the hooks with the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    WooOptica_Loader    Orchestrates the hooks of the plugin.
-	 */
-	public function get_loader() {
-		return $this->loader;
-	}
-
-	/**
-	 * Retrieve the version number of the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The version number of the plugin.
-	 */
-	public function get_version() {
-		return $this->version;
-	}
-
+    /**
+     * Retrieve the version number of the plugin.
+     *
+     * @since     1.0.0
+     * @return    string    The version number of the plugin.
+     */
+    public function get_version()
+    {
+        return $this->version;
+    }
 }
